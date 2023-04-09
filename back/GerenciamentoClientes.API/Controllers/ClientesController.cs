7ee84fc7296a71268c19cd8be7a2a9c9274c9a9e
danilo.silva <danilo.silva@msn.com>
2023-04-09 18:29:44 -0300

    Projeto Front

---
+++ back/GerenciamentoClientes.API/Controllers/ClientesController.cs
@@ -0,0 +1,25 @@
+﻿using GerenciamentoClientes.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GerenciamentoClientes.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClientesController : ControllerBase
+    {
+        private static List<Cliente> _clientes = new List<Cliente>();
+
+        [HttpGet]
+        public IActionResult ObterClientes()
+        {
+            return Ok(_clientes);
+        }
+
+        [HttpPost]
+        public IActionResult AdicionarCliente([FromBody] Cliente cliente)
+        {
+            _clientes.Add(cliente);
+            return Ok();
+        }
+    }
+}
---
+++ back/GerenciamentoClientes.API/Models/Cliente.cs
@@ -0,0 +1,11 @@
+﻿namespace GerenciamentoClientes.API.Models;
+
+public class Cliente
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Cpf { get; set; }
+    public DateTime DataNascimento { get; set; }
+    public string Celular { get; set; }
+    public string Email { get; set; }
+}
