5fcb4008aa47add8f15167b884d51681bddf7dc5
danilo.silva <danilo.silva@msn.com>
2023-04-09 18:58:10 -0300

    Documentação da Program

--- back/GerenciamentoClientes.API/Program.cs
+++ back/GerenciamentoClientes.API/Program.cs
@@ -1,8 +1,10 @@
+// Cria um novo WebHostBuilder a partir dos argumentos de entrada
 var builder = WebApplication.CreateBuilder(args);
 
+// Adiciona o serviço de controladores à coleção de serviços do aplicativo
 builder.Services.AddControllers();
 
-
+// Adiciona o serviço CORS à coleção de serviços do aplicativo e define uma política padrão que permite qualquer origem, método e cabeçalho
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(builder =>
@@ -13,15 +15,20 @@ builder.Services.AddCors(options =>
     });
 });
 
+// Cria uma instância do aplicativo a partir do WebHostBuilder
 var app = builder.Build();
 
+// Habilita o roteamento de solicitações HTTP
 app.UseRouting();
 
+// Habilita o middleware CORS para permitir solicitações de qualquer origem
 app.UseCors();
 
+// Configura o pipeline de solicitações HTTP para mapear solicitações para controladores
 app.UseEndpoints(endpoints =>
-    {
-        endpoints.MapControllers();
-    });
+{
+    endpoints.MapControllers();
+});
 
+// Inicia o aplicativo e espera por solicitações HTTP
 app.Run();
